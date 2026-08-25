cca895c43790e367a233f94aa08b91de4635ca4a
EmmanuelJGutierrez <emmajgutierrezwm@gmail.com>
2026-08-25 20:09:48 -0300

    feat: agrega interfaces IRepository e IUnitOfWork en Application

---
+++ Application/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿
+using Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Application.Interfaces
+{
+    public interface IRepository<T> where T: BaseEntity
+    {
+        Task<T?> GetAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync (T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<T> Repository<T>() where T : Domain.Entities.BaseEntity;
+
+        Task<int> SaveChangesAsync();
+
+        Task BeginTransactionAsync();
+        Task CommitAsync();
+        Task RollbackAsync();
+    }
+}
