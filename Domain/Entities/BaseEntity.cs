ed1a458ab8fb67edeead438c9819cac4a94a200c
EmmanuelJGutierrez <emmajgutierrezwm@gmail.com>
2026-08-25 15:15:25 -0300

    docs: agrega comentarios a BaseEntity

--- Domain/Entities/BaseEntity.cs
+++ Domain/Entities/BaseEntity.cs
@@ -1,7 +1,14 @@
 ﻿namespace Domain.Entities
 {
+
+    /*
+     * Clase base para todas las entidades del dominio.
+     * Contiene propiedades comunes que todas las entidades deben tener (Id).
+     * es necesaria para que (IRepository<T>) funcione correctamente en cualquier entidad.
+     */
     public abstract class BaseEntity
     {
+        // identificador unico de la entidad (Primary Key)
         public int Id { get; set; }
     }
 }
