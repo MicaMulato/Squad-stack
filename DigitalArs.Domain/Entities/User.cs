3820f428be26d96970e2cee8e293f3e2058e7f55
EmmanuelJGutierrez <emmajgutierrezwm@gmail.com>
2026-08-27 17:08:31 -0300

    Revert "Feature/hu 05 data seeding"

--- DigitalArs.Domain/Entities/User.cs
+++ DigitalArs.Domain/Entities/User.cs
@@ -1,19 +1,18 @@
-using Microsoft.AspNetCore.Identity;
-
 namespace DigitalArs.Domain.Entities;
 
-public class User : IdentityUser<int>
+public class User : BaseEntity
 {
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
     public bool IsDeleted { get; set; } = false;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
-    // Foreign Key y propiedad de navegacion con Role (no se usa RoleId propio, se maneja via IdentityUserRole)
-    // Pero mantenemos la navegacion directa para simplificar consultas
+    // Foreign Key y propiedad de navegación con Role
     public int RoleId { get; set; }
     public Role? Role { get; set; }
 
-    // Propiedad de navegacion bidireccional con Account
+    // Propiedad de navegación bidireccional con Account
     public Account? Account { get; set; }
 }
